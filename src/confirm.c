ca60fda61100ce5051d9e2579c0a583ce41b317e
phedwin <phedwinsching@students.uonbi.ac.ke>
2025-04-16 11:48:50 +0300

    - FIX forever(for;;) IP discovery with a 30 seconds timeout  - AHHHH pull my hair
    Signed-off-by: phedwin <phedwinsching@students.uonbi.ac.ke>

--- src/confirm.c
+++ src/confirm.c
@@ -5,8 +5,9 @@ void ___confirm_broadcast_sent() {
 	int sockfd;
 	struct sockaddr_in addr, sender;
 	socklen_t sender_len = sizeof(sender);
-#define BUF_SIZE 1024
-	char buffer[BUF_SIZE];
+
+#define BUFFER_SIZE 1024
+	char buffer[BUFFER_SIZE];
 
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd < 0)
@@ -22,18 +23,40 @@ void ___confirm_broadcast_sent() {
 
 	printf("Listening for UDP broadcasts on port %d...\n", PORT);
 
-	for (;;) {
-		ssize_t len = recvfrom(sockfd, buffer, BUF_SIZE - 1, 0,
-				       (struct sockaddr *)&sender, &sender_len);
-		if (len < 0) {
-			perror("recvfrom");
-			continue;
-		}
+	// discovery should last 30 seconds
+	struct timeval timeout;
+	timeout.tv_sec = 30;
+	timeout.tv_usec = 0;
 
-		buffer[len] = '\0'; /*! clean string*/
-		printf("Received from %s:%d -> %s\n",
-		       inet_ntoa(sender.sin_addr), ntohs(sender.sin_port),
-		       buffer);
+	for (;;) {
+		fd_set read_fds;
+		FD_ZERO(&read_fds);
+		FD_SET(sockfd, &read_fds);
+		int ret;
+
+		if ((ret = select(sockfd + 1, &read_fds, Nil, Nil, &timeout)) <
+		    0)
+			goto defer;
+		else if (ret == 0) {
+			printf("Timeout reached\n");
+			break;
+		} else {
+			// we have something to read
+			if (FD_ISSET(sockfd, &read_fds)) {
+				ssize_t len = recvfrom(
+				    sockfd, buffer, BUFFER_SIZE, 0,
+				    (struct sockaddr *)&sender, &sender_len);
+				if (len < 0) {
+					perror("recvfrom");
+					continue;
+				}
+
+				buffer[len] = '\0';
+				printf("Received from %s:%d -> %s\n",
+				       inet_ntoa(sender.sin_addr),
+				       ntohs(sender.sin_port), buffer);
+			}
+		}
 	}
 
 	close(sockfd);
